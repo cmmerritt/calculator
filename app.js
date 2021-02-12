6e9c9597b468ee45b7b11dc4d5751cbab7bf7112
cmmerritt <christiane.merritt@gmail.com>
2021-02-12 09:50:01 -0800

    Refactor math functions

--- app.js
+++ app.js
@@ -2,15 +2,32 @@
 
 // set event listeners on each button, do math, and update DOM
 
+import { add, subtract, multiply, divide } from '../utilities.js';
 
 
-const firstAddNumberInput = document.getElementById('first-add-number');
+function initOperator(firstInputId, secondInputId, buttonId, resultsId, calc) {
+    const input1 = document.getElementById(firstInputId);
+    const input2 = document.getElementById(secondInputId);
+    const button = document.getElementById(buttonId);
+    const display = document.getElementById(resultsId);
+
+    button.addEventListener('click', () => {
+        display.textContent = calc(Number(input1.value), Number(input2.value));
+    });
+}
+
+initOperator('first-add-number', 'second-add-number', 'add-button', 'addition-display', add);
+initOperator('first-sub-number', 'second-sub-number', 'sub-button', 'sub-display', subtract);
+initOperator('first-mult-number', 'second-mult-number', 'mult-button', 'mult-display', multiply);
+initOperator('first-div-number', 'second-div-number', 'div-button', 'div-display', divide);
+
+/* const firstAddNumberInput = document.getElementById('first-add-number');
 const secondAddNumberInput = document.getElementById('second-add-number');
 const additionDisplay = document.getElementById('addition-display');
 const addButton = document.getElementById('add-button');
 
 addButton.addEventListener('click', () => {
-    additionDisplay.textContent = Number(firstAddNumberInput.value) + Number(secondAddNumberInput.value);
+    additionDisplay.textContent = add(Number(firstAddNumberInput.value), Number(secondAddNumberInput.value));
 });
 
 const firstSubNumberInput = document.getElementById('first-sub-number');
@@ -19,7 +36,7 @@ const subDisplay = document.getElementById('sub-display');
 const subButton = document.getElementById('sub-button');
 
 subButton.addEventListener('click', () => {
-    subDisplay.textContent = Number(firstSubNumberInput.value) - Number(secondSubNumberInput.value);
+    subDisplay.textContent = subtract(Number(firstSubNumberInput.value), Number(secondSubNumberInput.value));
 });
 
 const firstMultNumberInput = document.getElementById('first-mult-number');
@@ -28,7 +45,7 @@ const multDisplay = document.getElementById('mult-display');
 const multButton = document.getElementById('mult-button');
 
 multButton.addEventListener('click', () => {
-    multDisplay.textContent = Number(firstMultNumberInput.value) * Number(secondMultNumberInput.value);
+    multDisplay.textContent = multiply(Number(firstMultNumberInput.value), Number(secondMultNumberInput.value));
 });
 
 const firstDivNumberInput = document.getElementById('first-div-number');
@@ -37,5 +54,5 @@ const divDisplay = document.getElementById('div-display');
 const divButton = document.getElementById('div-button');
 
 divButton.addEventListener('click', () => {
-    divDisplay.textContent = Number(firstDivNumberInput.value) / Number(secondDivNumberInput.value);
-});
+    divDisplay.textContent = divide(Number(firstDivNumberInput.value), Number(secondDivNumberInput.value));
+}); */
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,63 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { add } from '../utilities.js';
+const testAdd = QUnit.test;
 
-const test = QUnit.test;
-
-test('time to test a function', (expect) => {
+testAdd('time to test add function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 4;
+    const y = 5;
+    const sum = 9;
+
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const result = add(x, y);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(result, sum);
+
 });
+
+import { subtract } from '../utilities.js';
+const testSubtract = QUnit.test;
+
+testSubtract('time to test subtract function', (expect) => {
+    const x = 5;
+    const y = 2;
+    const subtracted = 3;
+
+    const result = subtract(x, y);
+
+    expect.equal(result, subtracted);
+
+})
+
+import { multiply } from '../utilities.js';
+const testMultiply = QUnit.test;
+
+testMultiply('time to test multiply function', (expect) => {
+    const x = 5;
+    const y = 2;
+    const multiplied = 10;
+
+    const result = multiply(x, y);
+
+    expect.equal(result, multiplied);
+})
+
+import { divide } from '../utilities.js';
+const testDivide = QUnit.test;
+
+testDivide('time to test divide function', (expect) => {
+    const x = 6;
+    const y = 2;
+    const divided = 3;
+
+    const result = divide(x, y);
+
+    expect.equal(result, divided);
+})
+
